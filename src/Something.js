f353dfdee5c1a6838a995307908c42d319f3f348
frank02902 <frankneba92@gmail.com>
2024-03-13 05:22:39 +0100

    like button update

--- src/Something.js
+++ src/Something.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Classes from './Name.module.css'
-const header = () => {
-  return(
+class header extends Component {
+  state = {
+    count:0
+  }
+  btnclick = () => {
+    this.setState((prevState,prevProps) => {
+    return {count: prevState.count + 1}});
+    
+  }
+  render ()
+    {
+    return (
     <div className = {Classes.blog}>
     <h4> this is the current level</h4>
     <p>hello</p>
-<hr></hr>
-    
+    <hr></hr>
+    <p> {this.state.count}</p>
+    <button onClick={this.btnclick}> like </button>
     </div>
-    )
+    );
+  }
+  
 }
 
 export default header
